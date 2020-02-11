b4f4662bba9c42173dbea20d566ef2c89427806d
kalyankerpraveen@gmail.com <Praveen.Kumar30@mphasis.com>
2020-02-11 23:14:24 +0530

    included reactstrap, bootstrap & react-router

---
+++ icubps-ui/src/components/loginRegister/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <div className='card'>
+        <form>
+          <div>
+            <label></label>
+            <input></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
